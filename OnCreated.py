e16b5b9536c9fc23d9f7b46cf37d4f9cb29f8b0e
Neogene <neogene@gmail.com>
2018-05-08 22:38:58 +0200

    First version.

---
+++ OnCreated.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+#TILE: HOUDINI HELP TO COMMENT
+#AUTHOR: eng. ANDREA LEGANZA
+#HOUDINI VERSION: TESTED ON HOUDINI 16.0 AND 16.5
+#SCRIPT VERSION: 1.0
+#DESCRIPTION:
+# 1) place script inside  <$houdini>/scripts/ 
+# 2) restart Houdini
+# 3) create a geometry node
+# 4) inside it create any kind of node
+# 5) script will add for any new node as comment the headline taken from online documentation
+# NOTE: some scripts don't have headline or entire documentation 
+
+import hou
+import os
+import zipfile
+
+ZIPFOLDER = os.environ['HFS']+"/houdini/help/nodes.zip".replace("/",os.sep)
+ARCHIVE = zipfile.ZipFile(ZIPFOLDER, 'r')
+   
+def getHeader(path):
+    path = path.replace("operator:","")
+    path = path.lower()
+    nodeHelpContent = ARCHIVE.read(path+".txt")
+    splitted = nodeHelpContent.split("\"\"\"")
+    return splitted[1] if len(splitted)>1 else "Not found"
+
+def main(kwargs):
+
+    node = kwargs["node"]
+
+    if len(node.comment())==0 :
+        description = getHeader(node.type().defaultHelpUrl())
+        node.setComment(description)
+        node.setGenericFlag(hou.nodeFlag.DisplayComment,True)
+    
+
+main(kwargs)
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # HoudiniHelpToComment
-A script which auto populate comment with node headline
+A script which auto populate NEW nodes comment with online documentation headline
+
+## AUTHOR: ## 
+eng. ANDREA LEGANZA
+
+## TESTED VERSION/S: ## 
+HOUDINI 16/16.5
+
+## DESCRIPTION: ## 
+When learning Houdini it may be hard to undestand the flow of nodes and to take a sneak peak on the purpose of a single node: a user has to select right click on node -> help and wait for Houdini internal browser to load the help page. This script simply read node headline (the title) from the internal documentation ad uses to populate the node comment.
+
+## INSTALLATION: ##
+1. place script inside  <$houdini>/scripts/ 
+2. restart Houdini
+3. create a geometry node
+4. inside it create any kind of node
+5. script will add for any new node as comment the headline taken from online documentation
+
+## NOTE: ## 
+Some nodes don't have headline or the whole documentation file.
+
+## CONTRIBUTING: ##
+Please feel free to optimize the script and add/report missing nodes documentation. 
